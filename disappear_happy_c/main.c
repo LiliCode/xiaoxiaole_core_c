5a5637f5b0b71e3f24bf3f03db65fd27ac6e68eb
lili <lili2011050508@163.com>
2016-07-19 15:43:35 +0800

    Initial Commit

---
+++ disappear_happy_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  disappear_happy_c
+//
+//  Created by 圈圈科技 on 16/7/19.
+//  Copyright © 2016年 圈圈科技. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
